e00af7a9fba9ac71c0a00389ad0f998659d04507
Dong Zhengyu <1327293674@qq.com>
2025-02-19 21:04:59 +0800

    寒假进展

---
+++ example.py
@@ -0,0 +1,16 @@
+
+def add(a, b):
+    sum = a + b
+    return sum
+
+def sub(c, d):
+    diff = c - d
+    return diff
+
+def sum_all(a, b):
+    result = a + b
+    return result
+
+def sum_all_again(x, y):
+    z = x + y
+    return z
